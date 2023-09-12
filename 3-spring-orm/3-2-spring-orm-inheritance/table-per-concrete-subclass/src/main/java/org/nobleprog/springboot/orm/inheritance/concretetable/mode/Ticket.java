3a4f1980da7ac1ad80ef73205c131c7157c806c3
NobleProgUk23 <nobleproguk23@gmail.com>
2023-09-12 19:51:03 +0000

    todo implemented

--- 3-spring-orm/3-2-spring-orm-inheritance/table-per-concrete-subclass/src/main/java/org/nobleprog/springboot/orm/inheritance/concretetable/mode/Ticket.java
+++ 3-spring-orm/3-2-spring-orm-inheritance/table-per-concrete-subclass/src/main/java/org/nobleprog/springboot/orm/inheritance/concretetable/mode/Ticket.java
@@ -3,6 +3,9 @@ package org.nobleprog.springboot.orm.inheritance.concretetable.mode;
 import javax.persistence.*;
 
 //TODO: use table per class strategy
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Ticket {
 
     @Id
