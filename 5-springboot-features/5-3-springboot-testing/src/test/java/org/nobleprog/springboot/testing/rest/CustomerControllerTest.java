0971c3496b1b734b832d5a151dbe2c8751657d39
NobleProgUk23 <nobleproguk23@gmail.com>
2023-09-13 10:42:55 +0000

    todo implemented

--- 5-springboot-features/5-3-springboot-testing/src/test/java/org/nobleprog/springboot/testing/rest/CustomerControllerTest.java
+++ 5-springboot-features/5-3-springboot-testing/src/test/java/org/nobleprog/springboot/testing/rest/CustomerControllerTest.java
@@ -1,13 +1,22 @@
 package org.nobleprog.springboot.testing.rest;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.nobleprog.springboot.testing.sevice.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 
 @RunWith(SpringRunner.class)
@@ -16,6 +25,21 @@ class CustomerControllerTest {
 
     @MockBean
     CustomerService customerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testGetAllCustomerIds() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().json("[]"));
+
+        Mockito.verify(customerService,Mockito.times(1))
+                .getAllCustomerIds();
+
+    }
 
 
 
